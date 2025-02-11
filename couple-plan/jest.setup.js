3d1bbc03002dc3cd90d5ff3528f17525964fe862
ReoKojima <kojileo0178@gmail.com>
2025-02-11 14:49:38 +0900

    ログインページのユニットテストを作成

--- couple-plan/jest.setup.js
+++ couple-plan/jest.setup.js
@@ -1,17 +1,16 @@
 import '@testing-library/jest-dom'
 
-// 既存の console.error を上書きして、特定の警告メッセージをフィルタリングする
+// 既存の console.error をラップして、特定のエラーメッセージをフィルタリングする
 const originalConsoleError = console.error;
 
 console.error = (...args) => {
-  // 警告メッセージに「cannot be a child of」が含まれている場合は何もしない
+  // 「cannot be a child of」と「ログインエラー:」を含む場合は出力しない
   if (
     args.length > 0 &&
     typeof args[0] === 'string' &&
-    args[0].includes('cannot be a child of')
+    (args[0].includes('cannot be a child of') || args[0].includes('ログインエラー:'))
   ) {
     return;
   }
-  // その他のメッセージはそのまま出力
   originalConsoleError(...args);
 };
